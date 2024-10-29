dcbcb9546924fef23a3c7887f88e719c16d54b2d
Hugo <hugojavidferrero@gmail.com>
2024-10-29 11:51:20 +0100

    fxmls creados a falta de los metodos

--- src/main/java/org/hugo/ejercicioh/HelloApplication.java
+++ src/main/java/org/hugo/ejercicioh/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("EjercicioH.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
-        Image icon = new Image(getClass().getResourceAsStream("/Imagenes/cuaderno.png"));
+        Image icon = new Image(getClass().getResourceAsStream("/Imagenes/agenda.png"));
         stage.getIcons().add(icon);
         stage.setMaxWidth(840);
         stage.setMinWidth(565);
