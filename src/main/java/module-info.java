dbcbce6457402b0c8151754deaae2da4eef5982d
Hugo <hugojavidferrero@gmail.com>
2024-10-29 10:37:59 +0100

    Clase conexionBBDD creada sin javadocs

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module org.hugo.ejercicioh {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     opens org.hugo.ejercicioh to javafx.fxml;
     exports org.hugo.ejercicioh;
